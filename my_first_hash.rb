914f8858e583429080dfbf86d2285291c0524a23
Travis Fox <travis.fox90@gmail.com>
2020-06-28 10:44:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-your_hash = { "eggs" => "over easy"
+your_hash = { "eggs" => "over easy", "toast" => "whole wheat", "coffee" => "black" }
   
 end
 
